cb1aa7afa44a11bdd71063da61c5b2186e2d678a
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-04-09 20:04:11 +0200

    Add: Bootstrap and clear & prettify results

--- index.php
+++ index.php
@@ -47,27 +47,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotels</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>    
-    <table>
+    <table class="table">
         <thead>
             <tr>
                 <?php foreach($hotels[0] as $key=> $hotel) { ?>
-                    <td><?php 
+                    <th><?php 
                         if(str_contains($key, '_')){
-                            echo str_replace('_', ' ', $key);
+                            echo strtoupper(str_replace('_', ' ', $key));
                         }else{
-                            echo $key;
+                            echo strtoupper($key);
                         }
-                        ?></td>
+                        ?></th>
                 <?php } ?>
             </tr>
         </thead>
         <tbody>
-            <?php foreach($hotels as $hotel) { ?>
+            <?php foreach($hotels as $key => $hotel) { ?>
                 <tr>
-                    <?php foreach($hotel as $value){ ?>
-                        <td> <?php echo $value ?></td>
+                    <?php foreach($hotel as $property => $value){ ?>
+                        <td> <?php 
+                            if($property == 'distance_to_center'){
+                                echo $value . ' KM';
+                            }
+                            else if($property == 'parking') {
+                                if($value == true){
+                                    echo 'YES';
+                                }else {
+                                    echo 'NO';
+                                }
+                            }
+                            else {
+                                echo $value;
+                            }
+                            ?></td>
                     <?php } ?>
                 </tr>
             <?php } ?>
